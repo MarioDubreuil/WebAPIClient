5f9a284c6bae7aab25eaf150a1ed31ee9c06860e
MarioDubreuil <mario_dubreuil@hotmail.com>
2022-07-20 09:14:03 -0400

    Initial commit

---
+++ WebAPIClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http.Headers;
+
+namespace WebAPIClient
+{
+    internal class Program
+    {
+        private static readonly HttpClient client = new();
+        static async Task Main()
+        {
+            await ProcessRepositories();
+        }
+
+        private static async Task ProcessRepositories()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+
+            var msg = await stringTask;
+            Console.WriteLine(msg);
+        }
+    }
+}
